3227393d8166cbff8562e743afdc1fc96c38fa92
reggie <806262720@qq.com>
2020-08-09 16:07:13 +0800

    before cloning

--- changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/controller/exception/BaseExceptionHandler.java
+++ changgou_service/changgou_service_goods/src/main/java/com/changgou/goods/controller/exception/BaseExceptionHandler.java
@@ -2,6 +2,8 @@ package com.changgou.goods.controller.exception;
 
 import com.changgou.entity.Result;
 import com.changgou.entity.StatusCode;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,11 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 /**
  * 统一异常处理类
  */
-@RestControllerAdvice //定义全局异常处理
+@RestControllerAdvice //定义全局异常处理，可以省略@ControllerAdvice方法中的@ResponseBody
+//@ControllerAdvice
 public class BaseExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
-    @ResponseBody
+    //@ResponseBody
     public Result error(Exception e) {
         e.printStackTrace();
         return new Result(false, StatusCode.ERROR, e.getMessage()); //异常信息e.getMessage()
