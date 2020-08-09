d28300d28569c105940053c302302a13fddbf5fc
reggie <806262720@qq.com>
2020-08-10 05:36:55 +0800

    File services coding and testing completed 文件服务编码及测试完成

---
+++ changgou_service/changgou_service_file/src/main/java/com/changgou/controller/FileController.java
@@ -0,0 +1,52 @@
+package com.changgou.controller;
+
+import com.changgou.entity.Result;
+import com.changgou.pojo.FastDFSFile;
+import com.changgou.util.FastDFSClient;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@CrossOrigin
+@RequestMapping("file")
+public class FileController {
+
+    @PostMapping("upload")
+    public Result uploadFile(@RequestParam("file") MultipartFile file) { //@RequestParam与请求地址一致
+        String path = "";
+        try {
+            path = saveFile(file);
+            System.out.println(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new Result("文件上传成功", path);
+    }
+
+    /**
+     * @param multipartFile
+     * @return
+     * @throws IOException
+     */
+    public String saveFile(MultipartFile multipartFile) throws IOException {
+        //1. 获取文件名
+        String fileName = multipartFile.getOriginalFilename();
+        //2. 获取文件内容
+        byte[] content = multipartFile.getBytes();
+        //3. 获取文件扩展名
+        String ext = "";
+        if (fileName != null && !"".equals(fileName)) {
+            ext = fileName.substring(fileName.lastIndexOf(".") + 1);//不加1扩展名会包含.
+        }
+        //4. 创建文件实体类对象
+        FastDFSFile fastDFSFile = new FastDFSFile(fileName, content, ext);
+        //5. 上传
+        String[] uploadResults = FastDFSClient.upload(fastDFSFile);
+        //6. 拼接上传后的文件的完整路径和名字, uploadResults[0]为组名, uploadResults[1]为文件名称和路径
+        String path = FastDFSClient.getTrackerUrl() + uploadResults[0] + "/" + uploadResults[1];
+        //7. 返回
+        return path;
+    }
+}
