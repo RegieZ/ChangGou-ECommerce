e219d24f280bf1aec257d9f73276dcb807a0ae31
reggie <806262720@qq.com>
2020-08-11 20:33:43 +0800

    Gateway priority adjustment 网关优先级调整

--- changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/filter/UrlFilter.java
+++ changgou_gateway/changgou_gateway_system/src/main/java/com/changgou/filter/UrlFilter.java
@@ -21,8 +21,16 @@ public class UrlFilter implements GlobalFilter, Ordered {
         return chain.filter(exchange);
     }
 
+    //1--->/goods/brand
+    //2---->/brand
+    /*
+        配置文件中配置了路由，路由本质也是filter,并且路由filter的优先级是2
+        如果自定义的filter优先级>=2,那么先执行路由规则（仅仅是执行路由规则，并不会真正路由服务
+        只有所有filter执行完毕后再路由）
+        ，再执行自定义的filter，一般建议设置优先级小于2
+     */
     @Override
     public int getOrder() {
-        return 2;
+        return 1;
     }
 }
